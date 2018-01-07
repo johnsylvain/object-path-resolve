1ab8dcd1126fbff6266218ccea348ffc60d58ddb
John Sylvain <john.n.sylvain@gmail.com>
2018-01-07 13:55:50 -0600

    add build process

--- test.js
+++ test.js
@@ -11,4 +11,15 @@ describe('object-resolve', () => {
 
     expect(resolve(obj, 'a.b')).toBe(obj.a.b)
   })
+
+  it('should resolve array paths', () => {
+    const obj = {
+      a: [
+        { b: 'c1' },
+        { b: 'c2' }
+      ]
+    }
+
+    expect(resolve(obj, 'a[0].b')).toBe(obj.a[0].b)
+  })
 })
