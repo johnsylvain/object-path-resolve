ed0ba02e7d8be3aa4721d50043e4b019bbca7d83
John Sylvain <john.n.sylvain@gmail.com>
2018-01-07 14:11:00 -0600

    fix path resolution for bracket notation

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 module.exports = function resolve (obj, path) {
-  path = path.split('.')
-  let res = obj
-  for (let i = 0; i < path.length; i++) res = res[path[i]]
-  return res
+  path = path
+    .split(/[.\[\]]/g)
+    .filter(i => i !== '')
+    .map(i => isNaN(i) 
+      ? i.replace(/['"]+/g, '') 
+      : parseInt(i)
+    )
+    
+  let ref = obj
+  for (let i = 0; i < path.length; i++) ref = ref[path[i]]
+  return ref
 }
--- test.js
+++ test.js
@@ -22,4 +22,16 @@ describe('object-resolve', () => {
 
     expect(resolve(obj, 'a[0].b')).toBe(obj.a[0].b)
   })
+
+  it('should be able to use bracket notation to resolve object', () => {
+    const obj = {
+      'test-prop': {
+        'test-array': [
+          'one', 'two', 'three'
+        ]
+      } 
+    }
+
+    expect(resolve(obj, '["test-prop"]["test-array"][2]')).toBe('three')
+  })
 })
