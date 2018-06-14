ecf50da1db207fda0d17b36c78fd1fbd376422c6
John Sylvain <john.n.sylvain@gmail.com>
2018-06-14 10:30:25 -0500

    fix tests

--- index.ts
+++ index.ts
@@ -1,10 +1,10 @@
-export default function resolve(obj: object, path: string): object | undefined {
+export default function resolve(obj: any, path: string): object | undefined {
   const params: string[] = path
     .split(/[.\[\]]+/g)
     .filter(Boolean)
     .map((i: string) => i.replace(/^["'](.*)["']$/g, "$1"));
   try {
-    for (let i in params) obj = obj[path[i]];
+    for (let i in params) obj = obj[params[i]];
     return obj;
   } catch (e) {}
 }
---
+++ test.ts
@@ -0,0 +1,64 @@
+import resolve from "./index";
+
+describe("object-resolve", () => {
+  it("should resolve nested paths", () => {
+    const obj = {
+      a: {
+        b: "value"
+      }
+    };
+
+    expect(resolve(obj, "a.b")).toBe(obj.a.b);
+  });
+
+  it("should resolve array paths", () => {
+    const obj = {
+      a: [{ b: "c1" }, { b: "c2" }]
+    };
+
+    expect(resolve(obj, "a[0].b")).toBe(obj.a[0].b);
+  });
+
+  it("should be able to use bracket notation to resolve object", () => {
+    const obj = {
+      "test-prop": {
+        "test-array": ["one", "two", "three"]
+      }
+    };
+
+    expect(resolve(obj, '["test-prop"]["test-array"][2]')).toBe("three");
+  });
+
+  it("should ignore inner quotes", () => {
+    const obj = {
+      'test-"prop"-value': {
+        "test-array": ["one", "two", "three"]
+      }
+    };
+
+    expect(resolve(obj, '["test-"prop"-value"]["test-array"][2]')).toBe(
+      "three"
+    );
+    expect(resolve(obj, '[test-"prop"-value]["test-array"][2]')).toBe("three");
+  });
+
+  it("should return undefined if does not exist", () => {
+    const obj = {
+      test: 1
+    };
+
+    expect(resolve(obj, "nope")).toBeUndefined();
+    expect(resolve(obj, "nope.nope")).toBeUndefined();
+  });
+
+  it("should not mutate original object", () => {
+    const obj = {
+      param: "test"
+    };
+
+    const original = Object.assign({}, obj);
+    resolve(obj, "param");
+
+    expect(obj).toEqual(original);
+  });
+});
